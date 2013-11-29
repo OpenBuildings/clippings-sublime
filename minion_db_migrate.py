6c348b6e1e2f229cc8e597d7edc91f116edc802e
Ivan Kerin <ikerin@gmail.com>
2013-11-29 18:23:24 +0200

    add some prelimenary minion tasks

---
+++ minion_db_migrate.py
@@ -0,0 +1,10 @@
+import sublime, sublime_plugin, copy, os
+from .clippings import ClippingsModule
+
+class MinionDbMigrateCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit):
+
+		cl_module = ClippingsModule(self.view)
+
+		self.view.window().run_command('exec', {'cmd': ['vagrant ssh -c "cd {0}; minion db:migrate"'.format(cl_module.vagrant_dir)], 'shell': True, 'syntax': 'Packages/clippings-sublime/PHPUnitResult.tmLanguage', 'color_scheme': 'Packages/clippings-sublime/PHPUnitResult.tmTheme'} )
